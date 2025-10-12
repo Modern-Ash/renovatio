5363b78d537148b9d752d95cbbfde901b3ed7b84
faguero <faguero@gmail.com>
2025-10-12 17:46:20 -0300

    refactor(cobol): simplify DiffResult by using Lombok annotations

--- renovatio-shared/src/main/java/org/shark/renovatio/shared/domain/DiffResult.java
+++ renovatio-shared/src/main/java/org/shark/renovatio/shared/domain/DiffResult.java
@@ -2,42 +2,22 @@ package org.shark.renovatio.shared.domain;
 
 import java.util.Map;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 /**
  * Result of diff operation
  */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class DiffResult extends ProviderResult {
     private String unifiedDiff;
     private Map<String, Object> semanticDiff;
     private Map<String, Object> hunks;
 
-    public DiffResult() {
-    }
-
     public DiffResult(boolean success, String message) {
         super(success, message);
-    }
-
-    public String getUnifiedDiff() {
-        return unifiedDiff;
-    }
-
-    public void setUnifiedDiff(String unifiedDiff) {
-        this.unifiedDiff = unifiedDiff;
-    }
-
-    public Map<String, Object> getSemanticDiff() {
-        return semanticDiff;
-    }
-
-    public void setSemanticDiff(Map<String, Object> semanticDiff) {
-        this.semanticDiff = semanticDiff;
-    }
-
-    public Map<String, Object> getHunks() {
-        return hunks;
-    }
-
-    public void setHunks(Map<String, Object> hunks) {
-        this.hunks = hunks;
     }
 }
