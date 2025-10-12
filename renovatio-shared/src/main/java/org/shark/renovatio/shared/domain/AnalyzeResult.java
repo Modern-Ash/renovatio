09045d287f61b51ce1698b376daa168ea1bc166b
faguero <faguero@gmail.com>
2025-10-12 17:34:31 -0300

    refactor(java): simplify AnalyzeResult class using Lombok annotations

--- renovatio-shared/src/main/java/org/shark/renovatio/shared/domain/AnalyzeResult.java
+++ renovatio-shared/src/main/java/org/shark/renovatio/shared/domain/AnalyzeResult.java
@@ -2,10 +2,16 @@ package org.shark.renovatio.shared.domain;
 
 import java.util.Map;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * Result of analyze operation
  */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class AnalyzeResult extends ProviderResult {
     private Map<String, Object> ast;
     private Map<String, Object> dependencies;
@@ -13,50 +19,7 @@ public class AnalyzeResult extends ProviderResult {
     private Map<String, Object> data;
     private PerformanceMetrics performance;
 
-    public AnalyzeResult() {
-    }
-
     public AnalyzeResult(boolean success, String message) {
         super(success, message);
-    }
-
-    public Map<String, Object> getAst() {
-        return ast;
-    }
-
-    public void setAst(Map<String, Object> ast) {
-        this.ast = ast;
-    }
-
-    public Map<String, Object> getDependencies() {
-        return dependencies;
-    }
-
-    public void setDependencies(Map<String, Object> dependencies) {
-        this.dependencies = dependencies;
-    }
-
-    public Map<String, Object> getSymbols() {
-        return symbols;
-    }
-
-    public void setSymbols(Map<String, Object> symbols) {
-        this.symbols = symbols;
-    }
-
-    public Map<String, Object> getData() {
-        return data;
-    }
-
-    public void setData(Map<String, Object> data) {
-        this.data = data;
-    }
-
-    public PerformanceMetrics getPerformance() {
-        return performance;
-    }
-
-    public void setPerformance(PerformanceMetrics performance) {
-        this.performance = performance;
     }
 }
