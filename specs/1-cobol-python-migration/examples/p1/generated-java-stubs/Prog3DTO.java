b68b60fb0968d8720ade30256a616fa0fbf2056e
faguero <faguero@gmail.com>
2026-08-31 15:03:23 -0300

    chore: clean generated files, update gitignore

--- specs/1-cobol-python-migration/examples/p1/generated-java-stubs/Prog3DTO.java
+++
@@ -1,9 +0,0 @@
-package org.shark.renovatio.generated.cobol;
-
-/**
- * Data Transfer Object generated from COBOL program: Prog3
- */
-public class Prog3DTO {
-  public Prog3DTO() {
-  }
-}
--- specs/1-cobol-python-migration/examples/p1/generated-java-stubs/SamplecopybookDTO.java
+++
@@ -1,9 +0,0 @@
-package org.shark.renovatio.generated.cobol;
-
-/**
- * Data Transfer Object generated from COBOL program: Samplecopybook
- */
-public class SamplecopybookDTO {
-  public SamplecopybookDTO() {
-  }
-}
